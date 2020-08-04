2b4ae121a61d2e9e135fab8b7ccd81d369b8b4e9
lyp <512275868@qq.com>
2020-08-04 23:00:44 +0800

    初始化项目

---
+++ src/test/java/com/example/dddscaffold/DddScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddscaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddScaffoldApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

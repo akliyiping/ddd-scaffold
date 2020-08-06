ed3fcff859ade9ac73c1db6de91c3ca5600c2d2f
liyiping <liyiping@liweihandeMac-mini.local>
2020-08-06 18:33:50 +0800

    add code

--- src/main/java/com/example/context/plant/infrastructure/repository/CropRepositoryImpl.java
+++ src/main/java/com/example/context/plant/infrastructure/repository/CropRepositoryImpl.java
@@ -4,21 +4,30 @@ import com.example.context.plant.domain.crop.model.Crop;
 import com.example.context.plant.domain.crop.repository.CropRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class CropRepositoryImpl implements CropRepository {
+  final List<Crop> cropList = new ArrayList<>();
+
   @Override
   public Crop find(String id) {
-	return null;
+	return cropList.stream().filter(crop -> crop.getId().equals(id)).findFirst().orElse(null);
   }
 
   @Override
   public Crop save(Crop crop) {
-    return null;
+    cropList.add(crop);
+    return crop;
   }
 
   @Override
   public Crop update(Crop crop) {
-    return null;
+    Crop oldCrop = this.find(crop.getId());
+    oldCrop.setWeight(crop.getWeight());
+    oldCrop.setName(crop.getName());
+    return oldCrop;
   }
 
 }
